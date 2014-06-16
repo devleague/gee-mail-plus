0d37354d77bdf752132b072380f6a18aee879de5
Gitthere <ichi4g@gmail.com>
2014-06-16 12:56:10 -1000

    got date are to populate, but 'undefined' shows up instead of actual date

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+//STEP 3  page loads, via JavaScript access 
+//prepopulated data stored in window.geemails variable
+
+
+
  // STEP 6 RETURNS NEWLY CREATED MSG
 
+//gets messages
  setTimeout (function() {getNewMessage()}, 3000);
     console.log(getNewMessage());
+
+//puts messages in browser
+var putsMessagesIn = getNewMessage();
+$dates = $('dates');
+$.each(putsMessagesIn, function(index, putsMessagesIn) {
+  $('#dates').append('<div>' +putsMessagesIn.dates+'</div>');
+}); 
     
