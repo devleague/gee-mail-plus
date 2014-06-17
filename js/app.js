0361433966cc0079552223fad42a8d7e6ab4bc2f
Gitthere <ichi4g@gmail.com>
2014-06-16 14:52:52 -1000

    each column lists 10 'undefined' items, still not pulling any of the stored data for date, subject and sender.

--- js/app.js
+++ js/app.js
@@ -6,13 +6,24 @@
  // STEP 6 RETURNS NEWLY CREATED MSG
 
 //gets messages
- setTimeout (function() {getNewMessage()}, 3000);
+setTimeout (function() {getNewMessage()}, 3000);
     console.log(getNewMessage());
 
-//puts messages in browser
-var putsMessagesIn = getNewMessage();
-$dates = $('dates');
-$.each(putsMessagesIn, function(index, putsMessagesIn) {
-  $('#dates').append('<div>' +putsMessagesIn.dates+'</div>');
+//puts messages in browser according to
+//date, sender and subject
+//var subject = generateMessage();
+$dates = $('#dates');
+$.each(subject, function(index, subject) {
+  $('#dates').append('<div>' + subject.dates + '</div>');
 }); 
-    
+
+$senders = $('#senders');
+$.each(sender, function(index,sender) {
+  $('#senders').append('<div>' + sender.senders + "</div");
+});
+
+$subjects = $('#subjects');
+$.each(subject, function(index,subject) {
+  $('#subjects').append('<div>' + subject.senders + "</div");
+});    
+
