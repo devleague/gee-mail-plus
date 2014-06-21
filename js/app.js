68ab08812f52d14f1bc807b3c75d67311f2e66b9
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-21 11:47:00 -1000

    render messages working

--- js/app.js
+++ js/app.js
@@ -1,43 +1,21 @@
- /*
-          <li class="email">
-            <span class="sender">Sender</span>
-            <span class="subject">Subject</span>
-            <span class="date">Date</span>
-          </li>
- */
- $(document).ready(function(){
+(function () {
 
-    var $body = $('#body');
-    var $subject = $('#subject');
-    var $sender = $('#sender');
-    var $date = $('#date');
-    
     var inbox = $('#inbox');
-
-    $("span").each(function(index) {
-      console.log(index + $sender.text() + $subject.text() + $date.text() );
-    });
 
     $.each(geemails, function(index, obj){
       console.log(obj);
 
-      var span = $('<li class="email"> + <span class="sender"> ' + obj.sender + ' </span>   <span class="date"> ' + obj.date + ' </span>  <span class="subject"> ' + obj.subject + ' </span> </li>');
+    var li = $('<li><li>');
+      var date_field = $('<span>' + obj.date + '</span>');
+      var sender_field = $('<span>' + obj.sender + '</span>');
+      var subject_field = $('<span>' + obj.subject + '</span>');
 
+      li.append(date_field);
+      li.append(subject_field);
+      li.append(sender_field);
 
-      //li.addClass('email')
-
-      inbox.append(span);
-
-      $('.sender').html(geemails[index].sender);
-      $('.subject').html(geemails[index].subject);
-      $('.date').html(geemails[index].date);
-      // $('#body').html(geemails[index].body); //Do an onclick function to display//
-
-    // $('#subject').html(obj[]);
+      inbox.append(li);
   
-    });
+    }); // closes $.each(geemails)
 
-
-
-  });
-  
+})();  
