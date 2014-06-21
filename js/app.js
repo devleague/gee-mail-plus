694fe3e90b1a7cdd7686834350af7c56e9c08787
Gitthere <ichi4g@gmail.com>
2014-06-21 11:36:49 -1000

    mails populate every 3 seconds

--- js/app.js
+++ js/app.js
@@ -1,62 +1,62 @@
-//STEP 3  page loads, via JavaScript access 
-//prepopulated data stored in window.geemails variable
+(function() {
 
-// loadGeeMails(); ################
-// //console.log(window.geemails);
-
-// //puts messages in browser according to
-// //date, sender and subject
-// $dates = $('#dates');//create jQuery object
-// $.each(getNewMessage, function(index, getNewMessage) {// Iterate over a 
-//   //jQuery object, executing a function for each matched element.
-//   $('#dates').append('<div>' + message.date + '</div>');//For the id
-//   //dates, should append the date from message.date in function generateMessage(date)
-// }); 
-
-// $senders = $('#senders');
-// $.each(sender, function(index,sender) {
-//   $('#senders').append('<div>' + sender.senders + "</div");
-// });
-
-// $subjects = $('#subjects');
-// $.each(subject, function(index,subject) {
-//   $('#subjects').append('<div>' + subject.subjects + "</div");
-// });    ####################
+    console.log(getNewMessage());
+    for (var i = 0; i < geemails.length; i++) {
+        myMessage(geemails[i]);
+    }
 
 
-// STEP 6 RETURNS NEWLY CREATED MSG
-//gets messages
-var newMessage = setInterval(function() {displayNewMessage()}, 2000);
-  function displayNewMessage() {
-    var now = getRandomDate();
-    //console.log($('#dates'));
-    //console.log(document.getElementById('dates'));
-    document.getElementById('newEmailDate').innerHTML = now;
-};
+    var newMessage = setInterval(displayNewMessage, 3000);
+    function displayNewMessage() {
+        addMessageToEmailList(getNewMessage());
+    };
 
-var myMessage = getNewMessage();
-// $('emailContainer').append('<span>' + $('#senders').sender + '</span>');
-// $('emailContainer').append('<span>' + $('#subjects').subject + '</span>');
-// console.log(sender);
+    for (var i = 0; i < geemails.length; i++) {
+        addMessageToEmailList(geemails[i]);
+    };
 
-console.log(myMessage);
-    //make new html elements for emails to be populated#######
+    function addMessageToEmailList(message) {
+        var li = $('<li></li>');
+        var date_field = $('<span>' + message.date + '</span>');
+        var subject_field = $('<span>' + message.subject + '</span>');
+        var sender_field = $('<span>' + message.sender + '</span>');
 
-  for (var i = 0; i < geemails.length; i++) { 
-    $('#emailList').append('<li><span>' + geemails[i].date + '</span>' + '<span>' + geemails[i].sender + '</span>' + '<span>' + geemails[i].subject + '</span><li>');  
-    // console.log(geemails[i]);
-    // console.log("date: " + geemails[i].date);
-    // console.log("date: " + geemails[i].subject);
-    // console.log("date: " + geemails[i].sender);
-    //console.log("body: " + geemails[i].body);
-    // console.log(" ");
+        li.append(date_field);
+        li.append(subject_field);
+        li.append(sender_field);
 
-    // $('#senders').append('<span>' + sender + '</span>');
-    // $('#subjects').append('<span>' + subject + '</span>');
-  }
+        $('#emailList').append(li);
+    };
+
+})();
+
+ //        //console.log($('#dates'));
+ //        //console.log(document.getElementById('dates'));
+ //        $('#newEmailDate').innerHTML = now;
+ //    };
+
+ //    var myMessage = getNewMessage();
+ //    // $('emailContainer').append('<span>' + $('#senders').sender + '</span>');
+ //    // $('emailContainer').append('<span>' + $('#subjects').subject + '</span>');
+ //    // console.log(sender);
+
+ //    console.log(myMessage);
+ //        //make new html elements for emails to be populated#######
+
+ //      for (var i = 0; i < geemails.length; i++) { 
+ //        $('#emailList').append('<li><span>' + geemails[i].date + '</span>' + '<span>' + geemails[i].sender + '</span>' + '<span>' + geemails[i].subject + '</span><li>');  
+ //        // console.log(geemails[i]);
+ //        // console.log("date: " + geemails[i].date);
+ //        // console.log("date: " + geemails[i].subject);
+ //        // console.log("date: " + geemails[i].sender);
+ //        //console.log("body: " + geemails[i].body);
+ //        // console.log(" ");
+
+ //        // $('#senders').append('<span>' + sender + '</span>');
+ //        // $('#subjects').append('<span>' + subject + '</span>');
+ //      }
   
- //SHOWING BODY OF MESSAGE WHEN IT IS CLICKED
+ // //SHOWING BODY OF MESSAGE WHEN IT IS CLICKED
 
-    
 
 
