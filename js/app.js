4037473e533aa71fe5dad085f926db808442541b
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-21 20:05:46 -1000

    setTimer function working

--- js/app.js
+++ js/app.js
@@ -3,15 +3,23 @@
 
     var inbox = $('#inbox');
 
+    setInterval(function () {
+      addMessageToInbox(getNewMessage());
+    }, 3000);
+
     for(var i= 0; i < geemails.length; i++){
+      addMessageToInbox(geemails[i]);
       console.log(i);
+    };
+
+    function addMessageToInbox(message){
 
       var li = $('<li class="mailMessageShow"></li>');
           
-      var sender_field = $('<span>' + geemails[i].sender + '</span>');
-      var date_field = $('<span>' + geemails[i].date + '</span>');
-      var subject_field = $('<span>' + geemails[i].subject + '</span>');
-      var body_field = $('<p>' + geemails[i].body + '</p>')
+      var sender_field = $('<span>' + message.sender + '</span>');
+      var date_field = $('<span>' + message.date + '</span>');
+      var subject_field = $('<span>' + message.subject + '</span>');
+      var body_field = $('<p>' + message.body + '</p>')
 
       li.append(sender_field);
       li.append(subject_field);
@@ -19,10 +27,11 @@
       li.append(body_field);
       
       inbox.append(li);
-  
-    } // closes $.each(geemails)
+
+    }; // ends function addMessageToInbox 
 
   $('.mailMessageShow').click(function() {
     $(this).find( "p" ).toggle( "slow" );
   }); 
+
 })();  
