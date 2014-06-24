4208bb663030dccf55d4ffde548fe730263a69ff
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-23 17:29:42 -1000

    -Hover highlight

--- js/app.js
+++ js/app.js
@@ -11,36 +11,40 @@
 
   function metaContent(message){
 
-  var li = $('<li></li>');
-        
-  var sender_field = $('<span>' + message.sender + '</span>');
-  var date_field = $('<span>' + message.date + '</span>');
-  var subject_field = $('<span>' + message.subject + '</span>');
-  var body_field = $('<p>' + message.body + '</p>')
+    var li = $('<li></li>');
+          
+    var sender_field = $('<span>' + message.sender + '</span>');
+    var date_field = $('<span>' + message.date + '</span>');
+    var subject_field = $('<span>' + message.subject + '</span>');
+    var body_field = $('<p>' + message.body + '</p>')
 
-  li.append(sender_field);
-  li.append(subject_field);
-  li.append(date_field);
+    li.append(sender_field);
+    li.append(subject_field);
+    li.append(date_field);
 
-  li.click(function() {
-    $(this).find( "p" ).toggle( "slow" );
-    li.append(body_field);
-  });  
+    li.click(function() {
+      $(this).find( "p" ).toggle( "slow" );
+      li.append(body_field);
+    });  
 
-  inbox.append(li);
+    inbox.append(li);
+
+    li.addClass("message-row");
   
   } 
 
-  var counter = 10;
-  function inBoxCounter(){
-    counter++;
-    $("h1").replaceWith('<h1>' + "You have " + counter + " unread messages" + '</h1>');
-  }
+var counter = 10;
+function inBoxCounter(){
+  counter++;
+  $("h1").replaceWith('<h1>' + "You have " + counter + " unread messages" + '</h1>');
+}
 
-  setInterval(function(){
-    metaContent(getNewMessage());
-    inBoxCounter();
-  }
-  , 30000);
+setInterval(function(){
+  metaContent(getNewMessage());
+  inBoxCounter();
+}
+, 30000);
+
+
 
 })();  
