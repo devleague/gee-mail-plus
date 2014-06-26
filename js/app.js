c1dc577c671bc987a613f04a2c484f2efce2f237
Alex Conway <alexconway808@gmail.com>
2014-06-25 21:10:19 -1000

    hid mail messages with hide()

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
-for(var i = 0; i<geemails.length;i++){ 
+for(var i = 0; i<geemails.length; i++){ 
 	var mail=geemails[i]
 	console.log(mail.subject)
 	console.log(mail.date)
 	console.log(mail.sender)
 	console.log(mail.body)
 
-$("<h3>"+(mail.subject)+"</h3>").appendTo($("body"))
-$("<h3>"+(mail.date)+"</h3>").appendTo($("body"))
-$("<h3>"+(mail.sender)+"</h3>").appendTo($("body"))
-$("<h3>"+(mail.body)+"</h3>").appendTo($("body"))
+
+var emailContainer = $("<div></div>");
+var emailSender = $("<h3>"+ mail.sender +"</h3>");
+var emailSubject = $("<p>"+ mail.subject +"</p>");
+var emailDate = $("<p>"+ mail.date +"</p>");
+var emailBody = $("<p>"+ mail.body +"</p>").hide();
+
+
+$(emailContainer)
+	.append(emailSender)
+	.append(emailSubject)
+	.append(emailDate)
+	.append(emailBody)
+
+$(".messages").append(emailContainer);
+
+
+// $("<h3 id=\"email" + i + "\">" +(mail.sender)+"</h3>").appendTo($("body"))
+// $("<p>"+(mail.subject)+"</p>").appendTo($("body"))
+// $("<p>"+(mail.date)+"</p>").appendTo($("body"))
+// $("<h3>"+(mail.body)+"</h3>").appendTo($("body"))
 
 }
