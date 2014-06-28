d09a1a0baca4d3f532cfd2e812617100c0852c11
Alex Conway <alexconway808@gmail.com>
2014-06-28 10:03:38 -1000

    finishing touches and added comments to explain all code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 (function(){
 
-
+	//for loop to pull in the gee mails//
 	for(var i = 0; i<geemails.length; i++){ 
 		different(geemails[i]);
 	};
 
+	//function that pulls in the various parts of each email message by creating a DOM element//
 	function different(mail){
+		//these vars are storing DOM elements created with jquery
 		var emailContainer = $("<div class='email'></div>");
 		var emailSender = $("<h3 class='email_sender'>"+ mail.sender +"</h3>");
 		var emailSubject = $("<p class='email_subject'>"+ mail.subject +"</p>");
 		var emailDate = $("<p class='email_date'>"+ mail.date +"</p>");
 		var emailBody = $("<p class='email_body'>"+ mail.body +"</p>").hide();
 	
+		//appending each attribute to build the email block
 		$(emailContainer)
 			.append(emailSender)
 			.append(emailSubject)
@@ -21,9 +24,13 @@
 		$(".messages").append(emailContainer);
 
 	}
-		countEmail();
+
+	//count the emails when the page and emails initially load//
+	countEmail();
 
 	//upon clicking any part of emailContainer, find the email body and show it on the page//
+	//add click event listener to .messages DOM element, which then selects the second parameter .email, then use that 
+	//element inside the function to find and show the email body
 	$(".messages").on("click", ".email", function(){
 		$(this).find(".email_body").show();
 	});
@@ -34,10 +41,8 @@
 		document.getElementById("inbox_counter").innerHTML = totalEmail;
 	}
 
-	// Set a recurring function to via the JavaScript setTimeout function that 
-	// will call a getNewMessage function that we have created for you that will 
-	// return a newly created message with the same properties as your previous messages.
-	// create a function that calls getNewMessage using setinterval
+	// Set a recurring function to via the JavaScript setTimeout function that will call a getNewMessage function that we have created for you that will 
+	// return a newly created message with the same properties as your previous messages create a function that calls getNewMessage using setinterval
 	// the new messages are displaying in the console as Objects
 
  	setInterval(function(){
@@ -48,25 +53,4 @@
 
 })();
 
-
-
-
-
-
-
-
-
-//create an array to store if messages have been read or not//
-// var messages = [];
-
-// function mail(message){
-
-// message.read = null;
-
-// messages.push(message)
-
-// }
-
-// $("p").click(function (){
-// 	.show('messagebody');
 
